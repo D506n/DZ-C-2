9a51035e7be3aceefe3bb220bfe7b78715db400b
Dmitriy Medvedev <D506n@yandex.ru>
2023-05-02 20:03:04 +0300

    Коммит перед отправкой ДЗ

---
+++ DZ2 Zadan 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер дня: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day > 0 & Day <=7){
+    if (Day > 5){
+        Console.WriteLine ("Выходной");
+    }
+    else Console.WriteLine ("Рабочий");
+}
+else {
+    Console.WriteLine ("Введён не день недели");
+}
